801166f079f13c5c7f89420247fa7a86adbb4d26
Nathan Nash <nathan.r.nash@gmail.com>
2022-11-26 20:17:53 -0800

    dap start up warning
    If dap is not installed will get a warning even if
    optional dap command is set to nil. Check in const.lua
    if dap is available before using it.

--- lua/cmake-tools/const.lua
+++ lua/cmake-tools/const.lua
@@ -1,3 +1,11 @@
+local has_dap, dap = pcall(require, "dap")
+local dap_repl_open = nil
+
+if has_dap
+then
+  dap_repl_open = dap.repl.open
+end
+
 local const = {
   cmake_command = "/usr/bin/cmake",
   cmake_build_directory = "build",
@@ -8,7 +16,7 @@ local const = {
   cmake_show_console = "always", -- "always", "only_on_error"
   cmake_focus_on_console = false, -- true, false
   cmake_dap_configuration = { name = "cpp", type = "codelldb", request = "launch" },
-  cmake_dap_open_command = require("dap").repl.open,
+  cmake_dap_open_command = dap_repl_open,
   cmake_variants_message = {
     short = { show = true },
     long = { show = true, max_length = 40 }
