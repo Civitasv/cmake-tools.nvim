72b56386935964858ffca0eb05928eb66ba048e5
Erez Amihud <erezamihud@gmail.com>
2023-07-31 00:17:34 +0300

    notify fix

--- lua/cmake-tools/utils.lua
+++ lua/cmake-tools/utils.lua
@@ -112,13 +112,15 @@ function utils.deepcopy(orig, copies)
   return copy
 end
 
-function notify_update_line(line)
-  if line and line:match("^%[%s*(%d+)%s*%%%]") then -- only show lines containing build progress e.g [ 12%]
-    notification.notification.id = notification.notify( -- notify with percentage and message
-      line,
-      notification.notification.level,
-      { replace = notification.notification.id, title = "CMakeTools" }
-    )
+local notify_update_line = function(line)
+  if line ~= nil then
+    if line and line:match("^%[%s*(%d+)%s*%%%]") then -- only show lines containing build progress e.g [ 12%]
+      notification.notification.id = notification.notify( -- notify with percentage and message
+        line,
+        notification.notification.level,
+        { replace = notification.notification.id, title = "CMakeTools" }
+      )
+    end
   end
 end
 
