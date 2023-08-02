06b3270e782b527f064cd795de65ca92b57a79ce
Civitasv <hscivitasv@gmail.com>
2023-08-02 16:54:26 +0800

    escape characters for {, }, ?, ,

--- lua/cmake-tools/init.lua
+++ lua/cmake-tools/init.lua
@@ -1345,7 +1345,12 @@ function cmake.create_regenerate_on_save_autocmd()
 
   local pattern = {}
   for _, item in ipairs(cmake_files) do
-    table.insert(pattern, item)
+    local ss = tostring(item)
+    ss = ss:gsub("{", "\\{")
+    ss = ss:gsub("}", "\\}")
+    ss = ss:gsub("?", "\\?")
+    ss = ss:gsub(",", "\\,")
+    table.insert(pattern, ss)
   end
 
   local presets_file = presets.check()
