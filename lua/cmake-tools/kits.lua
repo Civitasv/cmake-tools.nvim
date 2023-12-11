645dbbf20bb135e1387f24afcc82c2cf994036b4
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-12-11 21:27:28 +0100

    don't try tor read kits when there is no file present

--- lua/cmake-tools/kits.lua
+++ lua/cmake-tools/kits.lua
@@ -23,7 +23,9 @@ function kits.parse(global_kits_path, cwd)
   local config = nil
 
   local file = findcfg() -- check for config file
-
+  if file == nil then
+    return config
+  end
   file = file:gsub("~", vim.fn.expand("~"))
   if file then -- if one is found ...
     if file:match(".*%.json") then -- .. and is a json file
