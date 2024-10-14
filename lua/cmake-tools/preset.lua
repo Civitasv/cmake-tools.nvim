e87b73928e27bfcc7211b8dc9a46852ca24981da
Øystein Walle <oystwa@gmail.com>
2024-10-14 14:24:32 +0200

    Don't make binaryDirExpanded relative
    It's made relative to nvim's cwd, not config.cwd and furthermore there
    is no point in making it relative in the first place.

--- lua/cmake-tools/preset.lua
+++ lua/cmake-tools/preset.lua
@@ -38,7 +38,6 @@ local function resolveBuildDir(self)
     return
   end
   self.binaryDirExpanded = expandMacro(self, self.binaryDir)
-  self.binaryDirExpanded = vim.fn.fnamemodify(self.binaryDirExpanded, ":.")
 end
 
 local function resolveCacheVariables(self)
