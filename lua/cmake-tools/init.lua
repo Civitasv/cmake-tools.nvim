15a095c151364baa4cddc553f121e8df087983a6
Rachel Powers <508861+Ryex@users.noreply.github.com>
2024-11-10 08:48:17 -0700

    append `build_options` last so backend args pass correctly (#263)
    append the `build_options` args last so that passing `"--", "<some backend arg>"` works correctly

--- lua/cmake-tools/init.lua
+++ lua/cmake-tools/init.lua
@@ -353,8 +353,6 @@ function cmake.build(opt, callback)
     }
   end
 
-  vim.list_extend(args, config:build_options())
-
   if opt.target ~= nil then
     vim.list_extend(args, { "--target", opt.target })
     vim.list_extend(args, fargs)
@@ -365,6 +363,8 @@ function cmake.build(opt, callback)
     vim.list_extend(args, { "--target", config.build_target })
     vim.list_extend(args, fargs)
   end
+
+  vim.list_extend(args, config:build_options())
 
   local env = environment.get_build_environment(config)
   local cmd = const.cmake_command
