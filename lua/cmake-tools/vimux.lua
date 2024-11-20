3aa3d1b937dca754ee78f3f0197c2e162f2bf0cf
amirt01 <amirtadros01@gmail.com>
2024-11-20 08:15:17 -0800

    reintroduce removed function after merge
    the terminal executor/runner seems to have been changed since the last
    time I rebased so I reintroduced the necessary functions to make my
    vimux implementation work.

--- lua/cmake-tools/vimux.lua
+++ lua/cmake-tools/vimux.lua
@@ -1,5 +1,6 @@
 local terminal = require("cmake-tools.terminal")
-
+local osys = require("cmake-tools.osys")
+local utils = require("cmake-tools.utils")
 ---@class vimux : terminal
 local _vimux = {
   id = nil,
@@ -14,7 +15,7 @@ function _vimux.close(opts)
 end
 
 function _vimux.run(cmd, env_script, env, args, cwd, opts, on_exit, on_output)
-  local full_cmd = terminal.prepare_cmd_for_run(cmd, env, args, cwd)
+  local full_cmd = _vimux.prepare_cmd_for_run(cmd, env, args, cwd)
   vim.fn.VimuxRunCommand(full_cmd)
   terminal.handle_exit(opts, on_exit, opts.close_on_exit)
 end
@@ -34,6 +35,40 @@ function _vimux.is_installed()
     return "Vimux plugin is missing, please install it"
   end
   return true
+end
+
+function _vimux.prepare_cmd_for_run(cmd, env, args, cwd)
+  local full_cmd = ""
+
+  -- Launch form executable's build directory by default
+  full_cmd = "cd " .. utils.transform_path(cwd) .. " &&"
+
+  if osys.iswin32 then
+    for k, v in pairs(env) do
+      full_cmd = full_cmd .. " set " .. k .. "=" .. v .. "&&"
+    end
+  else
+    for k, v in pairs(env) do
+      full_cmd = full_cmd .. " " .. k .. "=" .. v .. ""
+    end
+  end
+
+  full_cmd = full_cmd .. " " .. utils.transform_path(cmd)
+
+  if osys.islinux or osys.iswsl or osys.ismac then
+    full_cmd = " " .. full_cmd -- adding a space in front of the command prevents bash from recording the command in the history (if configured)
+  end
+
+  -- Add args to the cmd
+  for _, arg in ipairs(args) do
+    full_cmd = full_cmd .. " " .. arg
+  end
+
+  if osys.iswin32 then -- wrap in sub process to prevent env vars from being persited
+    full_cmd = 'cmd /C "' .. full_cmd .. '"'
+  end
+
+  return full_cmd
 end
 
 return _vimux
